cf7d5727ba6e627f9c599f6291912f4a298f2f71
Natiq <natiq00h2272@gmail.com>
2022-12-12 16:59:03 +0400

    Initial commit

---
+++ app/src/main/java/com/natiqhaciyef/nikeapp/presentation/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.natiqhaciyef.nikeapp.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.natiqhaciyef.nikeapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
