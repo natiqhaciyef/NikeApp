0051af34b69897dbb8288dfdff7a335db06aa244
Natiq <natiq00h2272@gmail.com>
2022-12-12 22:00:36 +0400

    Splash screen ui ready

---
+++ app/src/main/java/com/natiqhaciyef/nikeapp/presentation/fragment/NotificationFragment.kt
@@ -0,0 +1,27 @@
+package com.natiqhaciyef.nikeapp.presentation.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.natiqhaciyef.nikeapp.R
+import com.natiqhaciyef.nikeapp.databinding.FragmentNotificationBinding
+
+
+class NotificationFragment : Fragment() {
+    private lateinit var binding: FragmentNotificationBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_notification, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
