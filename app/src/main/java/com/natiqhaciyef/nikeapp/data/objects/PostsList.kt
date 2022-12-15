088a0c660bee4b939e9d2ff354e94f6c9597f1e6
Natiq <natiq00h2272@gmail.com>
2022-12-15 18:46:15 +0400

    ADs Recycler view ready to usage

---
+++ app/src/main/java/com/natiqhaciyef/nikeapp/data/objects/PostsList.kt
@@ -0,0 +1,18 @@
+package com.natiqhaciyef.nikeapp.data.objects
+
+import com.natiqhaciyef.nikeapp.data.model.PostModel
+
+object PostsList {
+    val postList = mutableListOf(
+        PostModel(1,"Air Jordan 13","nike_air_jordan_13" ,200),
+        PostModel(2,"Air Jordan 12 Retro","nike_air_jordan_12_retro" ,175),
+        PostModel(3,"Air Jordan 7 Retro","nike_air_jordan_7_retro_q" ,150),
+        PostModel(4,"Air Jordan 9 Retro","nike_air_jordan_9_retro" ,150),
+        PostModel(5,"Air Jordan XXXVII","nike_air_jordan_xxxvii" ,205),
+        PostModel(6,"Air Max 1 NH","nike_air_max_1_nh" ,150),
+        PostModel(7,"Air Zoom Flight 95","nike_air_zoom_flight_95" ,160),
+        PostModel(8,"Air Dunk High Retro","nike_dunk_high_retro_yellow" ,85),
+        PostModel(9,"Kyrie 5 Infinity","nike_kyrie_infinity_5" ,160),
+        PostModel(10,"LeBron 19 Low","nike_lebron_19_low" ,100)
+    )
+}
---
+++ app/src/main/java/com/natiqhaciyef/nikeapp/presentation/adapter/AdvertisementAdapter.kt
@@ -0,0 +1,34 @@
+package com.natiqhaciyef.nikeapp.presentation.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.natiqhaciyef.nikeapp.R
+import com.natiqhaciyef.nikeapp.data.model.AdvertisementModel
+import com.natiqhaciyef.nikeapp.databinding.RecyclerAdvertiseViewBinding
+
+class AdvertisementAdapter(val mContext: Context, var list: MutableList<AdvertisementModel>) :
+    RecyclerView.Adapter<AdvertisementAdapter.AdvertisementHolder>() {
+
+    inner class AdvertisementHolder(val binding: RecyclerAdvertiseViewBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdvertisementHolder {
+        val binding: RecyclerAdvertiseViewBinding = DataBindingUtil
+            .inflate(LayoutInflater.from(mContext), R.layout.recycler_advertise_view, parent, false)
+        return AdvertisementHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AdvertisementHolder, position: Int) {
+        val itemView = holder.binding
+        val ad = list[position]
+
+        itemView.adModel = ad
+        itemView.adImageView.setImageResource(mContext.resources
+            .getIdentifier(ad.image, "drawable", mContext.packageName))
+    }
+
+    override fun getItemCount(): Int = list.size
+
+}
