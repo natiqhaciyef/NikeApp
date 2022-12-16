62cfaf7403ed27b97be8f0cb360ee436981c2712
Natiq <natiq00h2272@gmail.com>
2022-12-16 17:14:27 +0400

    Bottom navigation bar activated

--- app/src/main/java/com/natiqhaciyef/nikeapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/natiqhaciyef/nikeapp/presentation/activity/MainActivity.kt
@@ -3,7 +3,10 @@ package com.natiqhaciyef.nikeapp.presentation.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.natiqhaciyef.nikeapp.R
 import com.natiqhaciyef.nikeapp.databinding.ActivityMainBinding
 import com.natiqhaciyef.nikeapp.presentation.behavior.BottomNavigationBehavior
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,5 +17,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNavBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.homeFragment -> {
+                    navigationFragments(R.id.homeFragment)
+                    true
+                }
+                R.id.userFragment -> {
+                    navigationFragments(R.id.userFragment)
+                    true
+                }
+                R.id.savedFragment -> {
+                    navigationFragments(R.id.savedFragment)
+                    true
+                }
+                R.id.notificationFragment -> {
+                    navigationFragments(R.id.notificationFragment)
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+
+    }
+
+    private fun navigationFragments(id: Int){
+        binding.fragmentContainerView.findNavController().navigate(id)
     }
 }
