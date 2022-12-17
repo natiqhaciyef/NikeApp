aede9d8a570edad4eb2667759f5f6c9a06c6c881
Natiq <natiq00h2272@gmail.com>
2022-12-17 16:26:54 +0400

    Color functionality with UI & Set DetailsFragment.kt

--- app/src/main/java/com/natiqhaciyef/nikeapp/presentation/adapter/PostAdapter.kt
+++ app/src/main/java/com/natiqhaciyef/nikeapp/presentation/adapter/PostAdapter.kt
@@ -2,6 +2,7 @@ package com.natiqhaciyef.nikeapp.presentation.adapter
 
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
