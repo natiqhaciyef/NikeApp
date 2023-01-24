6de8ffffe14de4b52a50c0ef57b773f4e3d050f5
Natiq <natiq00h2272@gmail.com>
2023-01-24 18:08:58 +0400

    Instrumental testing created for CartDaoTest.kt. Testing room db was successful

---
+++ app/src/androidTest/java/com/natiqhaciyef/nikeapp/roomdb/CartDaoTest.kt
@@ -0,0 +1,77 @@
+package com.natiqhaciyef.nikeapp.roomdb
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import com.natiqhaciyef.nikeapp.data.model.CartPost
+import com.natiqhaciyef.nikeapp.data.room.CartDao
+import com.natiqhaciyef.nikeapp.data.room.NikeDatabase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+// During testing you have to comment shirinking and minifying in gradle module
+
+@SmallTest
+@ExperimentalCoroutinesApi
+class CartDaoTest {
+    @get: Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: NikeDatabase
+    private lateinit var dao: CartDao
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(), NikeDatabase::class.java
+        ).allowMainThreadQueries().build()
+        dao = database.getCartDao()
+    }
+
+    @After
+    fun teardown() {
+        database.close()
+    }
+
+    @Test
+    fun insertCartTesting() = runBlockingTest {
+        val cart = CartPost(
+            id = 1201,
+            name = "AJ",
+            image = "empty",
+            imagePng = "empty.png",
+            price ="71.3",
+            category = "basketball",
+            colors = "black"
+        )
+        dao.insertToCart(cart)
+        delay(1500)
+        val list = dao.getAllCart()
+        assertThat(list).contains(cart)
+    }
+
+    @Test
+    fun deleteCartTesting() = runBlockingTest {
+        val cart = CartPost(
+            id = 1201,
+            name = "AJ",
+            image = "empty",
+            imagePng = "empty.png",
+            price ="71.3",
+            category = "basketball",
+            colors = "black"
+        )
+        dao.deleteFromCart(cart)
+        delay(1500)
+        val list = dao.getAllCart()
+        assertThat(list).doesNotContain(cart)
+    }
+}
